d48d3ab2b20989df1a067d5b4636bee7556c671f
voyagelDevMaster <157929778+voyagelDevMaster@users.noreply.github.com>
2024-10-05 16:41:06 +0000

    android files

---
+++ android/app/src/main/java/sn/impactsolution/wirdTidiane/MainActivity.java
@@ -0,0 +1,5 @@
+package sn.impactsolution.wirdTidiane;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'sn.impactsolution.wirdTidiane',
+  appId: 'sn.impactsolution.wirdkhadr',
   appName: 'Wird Khadr',
   webDir: 'www'
 };
